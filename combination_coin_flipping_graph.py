62362669faaa9dde389a5e74d9f97dbd3210b815
Benjamin <ashbau.benjam19@svvsd.org>
2018-01-09 20:42:48 -0700

    fixed graphing

--- combination_coin_flipping_graph.py
+++ combination_coin_flipping_graph.py
@@ -13,7 +13,9 @@ input_num_coins = int(input("enter number of coins:"))
 def graph_var_numcoins():
     probs = []
     print("calculating with variable number of coins...")
-    sleep(2)
+    sleep(.8)
+    for i in range(0, input_num_heads + 1):
+        probs.append(0)
     for i in range(input_num_heads, input_max_coins + 1):
         prob = calculate(i, input_num_heads, input_prob_head)
         print(str(i) + " " + str(prob))
@@ -22,7 +24,7 @@ def graph_var_numcoins():
 def graph_var_numheads():
     probs = []
     print("calculating with variable number of heads turning up...")
-    sleep(2)
+    sleep(.8)
     for i in range(0, input_num_coins + 1):
         prob = calculate(input_num_coins, i, input_prob_head)
         print(str(i) + " " + str(prob))
@@ -31,12 +33,12 @@ def graph_var_numheads():
 def graph_var_probhead():
     probs = []
     print("calculating with variable probability of a head turning up...")
-    sleep(2)
+    sleep(.8)
     for i in range(0, 101):
         prob = calculate(input_num_coins, input_num_heads, i/100)
         print(str(i) + " " + str(prob))
         probs.append(prob)
-    plt.plot(probs, label="variable probability of a head turning up")
+    plt.plot(probs, label="variable probability of a head turning up(percentage)")
 
 def calculate(num_coins, num_heads, prob_head):
     a = (prob_head**num_heads * (1 - prob_head)**(num_coins - num_heads))
@@ -52,9 +54,11 @@ graph_var_numcoins()
 graph_var_numheads()
 graph_var_probhead()
 print("done.")
+answer = calculate(input_num_coins, input_num_heads, input_prob_head)
+print(str(answer) + " or " + str(Fraction(answer)))
 sleep(1)
-plt.legend()
+plt.legend(loc='upper right')
 plt.text(50, 0.01, "coins: " + str(input_num_coins) + ", heads: " + str(input_num_heads) + ", prob of a head: " + str(input_prob_head))
-plt.show()
+plt.show("me")
 print("goodbye!")
     
